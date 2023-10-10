f32c7f0e49414b2502b90799e075380b814a5c42
puzzlecollector <puzzlecollector1123581321@gmail.com>
2023-10-10 19:10:25 +0900

    settings 변경

--- config/settings/base.py
+++ config/settings/base.py
@@ -16,7 +16,7 @@ LOGIN_REDIRECT_URL = "/"
 LOGOUT_REDIRECT_URL = "/"
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
+BASE_DIR = Path(__file__).resolve().parent.parent.parent
 
 
 # Quick-start development settings - unsuitable for production
