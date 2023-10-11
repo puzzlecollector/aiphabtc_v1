a672a3e767adcbad67e86d4f7a504e8b0dab7fa9
puzzlecollector <puzzlecollector1123581321@gmail.com>
2023-10-11 17:54:19 +0900

    changed db to postgre

--- config/settings/prod.py
+++ config/settings/prod.py
@@ -5,3 +5,14 @@ ALLOWED_HOSTS = ["43.201.187.232", "www.aiphabtc.com"]
 STATIC_ROOT = BASE_DIR / "static/"
 STATICFILES_DIRS = []
 DEBUG = False
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'aiphabtc',
+        'USER': 'dbmasteruser',
+        'PASSWORD': '9^Mmi9x&gdaLop5C8|X`0ZUjUxPq42cv',
+        'HOST': 'ls-c734f681295dcc3ef154bf196883ce0d354c1c4f.cxmhq2eoxqys.ap-northeast-2.rds.amazonaws.com',
+        'PORT': '5432',
+    }
+}
