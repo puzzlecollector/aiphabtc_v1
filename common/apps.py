1fb640524bd7665dcdcaaafb29fb9cb0863453be
puzzlecollector <puzzlecollector1123581321@gmail.com>
2023-10-15 18:22:29 +0900

    signal added

--- common/apps.py
+++ common/apps.py
@@ -4,3 +4,5 @@ from django.apps import AppConfig
 class CommonConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'common'
+    def ready(self):
+        import common.signals
