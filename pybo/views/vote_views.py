97eb2059a1cee83ff8b398f16bdbabe62f6b73c7
puzzlecollector <puzzlecollector1123581321@gmail.com>
2023-10-19 16:45:47 +0900

    prevent double voting for points

--- pybo/views/vote_views.py
+++ pybo/views/vote_views.py
@@ -11,15 +11,18 @@ def vote_question(request, question_id):
     if request.user == question.author:
         messages.error(request, '본인의 글은 추천할 수 없어요!')
     else:
-        question.voter.add(request.user)
-        # Update scores
-        author_profile = question.author.profile
-        author_profile.score += 1  # author receives 2 points for a like
-        author_profile.save()
+        if request.user not in question.voter.all():
+            question.voter.add(request.user)
+            # Update scores
+            author_profile = question.author.profile
+            author_profile.score += 1  # author receives 2 points for a like
+            author_profile.save()
 
-        user_profile = request.user.profile
-        user_profile.score += 1  # user gives 1 point for a like
-        user_profile.save()
+            user_profile = request.user.profile
+            user_profile.score += 1  # user gives 1 point for a like
+            user_profile.save()
+        else:
+            messages.error(request, '이미 추천한 글입니다.')
 
     return redirect('pybo:detail', question_id=question.id)
 
@@ -29,14 +32,18 @@ def vote_answer(request, answer_id):
     if request.user == answer.author:
         messages.error(request, '본인이 작성한 글은 추천할 수 없습니다.')
     else:
-        answer.voter.add(request.user)
-        # Update scores
-        author_profile = answer.author.profile
-        author_profile.score += 1  # author receives 2 points for a like
-        author_profile.save()
+        if request.user not in answer.voter.all():
+            answer.voter.add(request.user)
+            # Update scores
+            author_profile = answer.author.profile
+            author_profile.score += 1  # author receives 2 points for a like
+            author_profile.save()
 
-        user_profile = request.user.profile
-        user_profile.score += 1  # user gives 1 point for a like
-        user_profile.save()
+            user_profile = request.user.profile
+            user_profile.score += 1  # user gives 1 point for a like
+            user_profile.save()
+        else:
+            messages.error(request, '이미 추천한 글입니다.')
 
     return redirect('pybo:detail', question_id=answer.question.id)
+
