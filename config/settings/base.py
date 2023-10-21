7ed0e9b994050979bdbdeaa51d288df994f05746
puzzlecollector <puzzlecollector1123581321@gmail.com>
2023-10-21 16:03:00 +0900

    hide openai api key

--- config/settings/base.py
+++ config/settings/base.py
@@ -140,5 +140,4 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
 
-# OPENAI_API_KEY = os.environ.get('OPENAI_API_KEY')
-OPENAI_API_KEY = "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"
+OPENAI_API_KEY = os.environ.get('OPENAI_API_KEY')
