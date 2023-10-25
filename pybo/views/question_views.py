8ab2dbe9ad953ce0c672472ed3303887c800fa27
puzzlecollector <puzzlecollector1123581321@gmail.com>
2023-10-25 17:25:08 +0900

    cached text the user was working on

--- pybo/views/question_views.py
+++ pybo/views/question_views.py
@@ -27,6 +27,7 @@ def question_create(request, board_name=None):
                 word_count = len(content.split())
                 if word_count < 55:
                     messages.error(request, "내용은 최소 55단어 이상이어야 합니다!")
+                    request.session["stored_form_data"] = request.POST
                     return redirect('pybo:question_create', board_name=board_name)
                 crypto = request.POST.get("crypto")
                 duration = request.POST.get("duration")
@@ -37,12 +38,15 @@ def question_create(request, board_name=None):
                     price_change_value = float(price_change.strip('%'))
                 except ValueError:
                     messages.error(request, "올바르지 않은 가격 변동 값입니다!")
+                    request.session["stored_form_data"] = request.POST
                     return redirect('pybo:question_create', board_name=board_name)
                 if direction == "bullish" and price_change_value < 0:
                     messages.error(request, "상승 추세를 위한 가격 변동은 0% 이상이어야 합니다!")
+                    request.session["stored_form_data"] = request.POST
                     return redirect('pybo:question_create', board_name=board_name)
                 elif direction == "bearish" and price_change_value > 0:
                     messages.error(request, "하락 추세를 위한 가격 변동은 0% 이하이어야 합니다!")
+                    request.session["stored_form_data"] = request.POST
                     return redirect('pybo:question_create', board_name=board_name)
 
                 last_prediction = Question.objects.filter(
@@ -67,6 +71,7 @@ def question_create(request, board_name=None):
                     if timezone.now() < end_time:
                         messages.error(request,
                                        '해당 코인에 대해서 이 기간 동안 이전 예측이 끝나기 전까지 다른 예측을 게시할 수 없습니다!')
+                        request.session["stored_form_data"] = request.POST
                         return redirect('pybo:question_create', board_name=board_name)
                     else:
                         subject = f"[{crypto}][{duration}][{direction}][{price_change}] {subject}"
@@ -80,16 +85,28 @@ def question_create(request, board_name=None):
                 word_count = len(content.split())
                 if word_count < 55:
                     messages.error(request, "내용은 최소 55단어 이상이어야 합니다!")
+                    request.session["stored_form_data"] = request.POST
                     return redirect('pybo:question_create', board_name=board_name)
 
             question.save()
+
+            if 'stored_form_data' in request.session:
+                del request.session['stored_form_data']
 
             if board_name:
                 return redirect('pybo:board_view', board_name=board_name)
             else:
                 return redirect("pybo:index")
+        else:
+            # store the form data in the session in case of form validation failure
+            request.session["stored_form_data"] = request.POST
+            return redirect('pybo:question_create', board_name=board_name)
     else:
-        form = QuestionForm()
+        if 'stored_form_data' in request.session:
+            form = QuestionForm(request.session['stored_form_data'])
+            del request.session['stored_form_data']
+        else:
+            form = QuestionForm()
     context = {"form": form}
     if board_name == "perceptive":
         context["is_perceptive_board"] = True
@@ -114,11 +131,21 @@ def question_modify(request, question_id):
                 word_count = len(content.split())
                 if word_count < 55:
                     messages.error(request, "내용은 최소 55단어 이상이어야 합니다!")
+                    request.session["stored_form_data"] = request.POST
                     return redirect('pybo:question_modify', question_id=question.id)
             question.save()
+            if 'stored_form_data' in request.session:
+                del request.session['stored_form_data']
             return redirect('pybo:detail', question_id=question.id)
+        else:
+            request.session["stored_form_data"] = request.POST
+            return redirect('pybo:question_modify', question_id=question.id)
     else:
-        form = QuestionForm(instance=question)
+        if 'stored_form_data' in request.session:
+            form = QuestionForm(request.session["stored_form_data"])
+            del request.session["stored_form_data"]
+        else:
+            form = QuestionForm(instance=question)
     context = {'form': form}
     return render(request, 'pybo/question_form.html', context)
 
